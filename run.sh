8f1b9c17f70c654ad3c7c644edf2389df3d88bcc
soniharish2018 <165396204+soniharish2018@users.noreply.github.com>
2024-08-25 23:36:51 +0530

    nvm

--- run.sh
+++ run.sh
@@ -22,7 +22,10 @@ mv jiotv_go-linux-amd64 jtv
 
 chmod +x jtv
 
-apt-get install nodejs -y
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 node -v
 
