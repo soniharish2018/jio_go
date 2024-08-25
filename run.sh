63c1b22076a4ca3e763aacc1e2ecee3760ed84e2
soniharish2018 <165396204+soniharish2018@users.noreply.github.com>
2024-08-25 23:40:28 +0530

    nbm

--- run.sh
+++ run.sh
@@ -25,7 +25,9 @@ chmod +x jtv
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
 node -v
 
