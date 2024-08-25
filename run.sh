5af7a88b29fb3df99d432a5b314378cb84d48a67
soniharish2018 <165396204+soniharish2018@users.noreply.github.com>
2024-08-25 23:39:18 +0530

    ddddd

--- run.sh
+++ run.sh
@@ -24,8 +24,8 @@ chmod +x jtv
 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
 
 node -v
 
