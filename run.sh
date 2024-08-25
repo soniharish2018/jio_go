810223e92ada6ca46e2ac6448caf694da7091374
soniharish2018 <165396204+soniharish2018@users.noreply.github.com>
2024-08-25 22:52:00 +0530

    asdd

--- run.sh
+++ run.sh
@@ -2,8 +2,12 @@ echo "Running"
 
 wget https://github.com/rabilrbl/jiotv_go/releases/latest/download/jiotv_go-linux-arm64
 
+echo "Move"
 mv jiotv_go-linux-arm64 jiotv
 
+echo "Execute"
 chmod +x jiotv
 
+
+echo "Run"
 ./jiotv run
